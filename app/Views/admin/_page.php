2e339a1a262a3f80214e10a18458ef46b9e0a00b
Piches Srisuk <picheat.ches@gmail.com>
2026-08-22 09:37:36 +0700

    refactor(views): ลบ _page.php ของ admin/user ที่ไม่มี controller เรียกแล้ว

--- app/Views/admin/_page.php
+++
@@ -1,5 +0,0 @@
-<?= $this->extend('layouts/admin') ?>
-
-<?= $this->section('content') ?>
-<?= $this->include('templates/_coming_soon', ['pageTitle' => $pageTitle ?? '']) ?>
-<?= $this->endSection() ?>
--- app/Views/user/_page.php
+++
@@ -1,5 +0,0 @@
-<?= $this->extend('layouts/user') ?>
-
-<?= $this->section('content') ?>
-<?= $this->include('templates/_coming_soon', ['pageTitle' => $pageTitle ?? '']) ?>
-<?= $this->endSection() ?>
